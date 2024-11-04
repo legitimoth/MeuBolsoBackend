46c84ef7612b6d4a9d62b9ea64e250f98d8a8aea
Thaynner Bastos <cast.thaynner.utic@sebrae.com.br>
2024-11-03 21:57:20 -0300

    ✅ Done: Enviando migrations (que por algumas razão não foi versionado 😡)

---
+++ Migrations/20241103235329_AttTagRmvCor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeuBolsoBackend
+{
+    /// <inheritdoc />
+    public partial class AttTagRmvCor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cor",
+                table: "Tags");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cor",
+                table: "Tags",
+                type: "character varying(7)",
+                maxLength: 7,
+                nullable: true);
+        }
+    }
+}
